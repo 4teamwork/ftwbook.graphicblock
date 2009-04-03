579441d617408534f7aa78fba958f6f8ba0fa6ac
Jonas Baumann <j.baumann@4teamwork.ch>
2009-04-03 13:44:45 +0000

    Issue #313 Inhaltstyp Grafikblock für Einbinden von Grafiken in PDF added latex view

---
+++ izug/graphicblock/latex/graphicblock.py
@@ -0,0 +1,56 @@
+
+from izug.bibliothek.latex.ctconverter import ZugCTConverter
+
+class GraphicBlockLatexConverter(ZugCTConverter):
+    
+    def __call__(self, context, view):
+        super(GraphicBlockLatexConverter, self).__call__(context, view)
+        latex = []
+        write = lambda *a:[latex.append(x) for x in a]
+        graphic = context.getGraphic()
+        # parse arguments
+        if self.context.width==100:
+            command = 'figure'
+            width = r'\columnwidth'
+        else:
+            command = 'wrapfigure'
+            width = r'%f\columnwidth' % (self.context.width / float(100))
+        # includegraphics options
+        include_options = [
+                r'width=%s' % width,
+        ]
+        trim = {
+            'left' : self.context.trim_left,
+            'right' : self.context.trim_right,
+            'top' : self.context.trim_top,
+            'bottom' : self.context.trim_bottom,
+        }
+        if len(filter(lambda x:x>0, trim.values()))>0:
+            # at least one side must be trimmed, we have to specify
+            # each side and add "clip" option
+            include_options.append('clip')
+            include_options.append('trim=%s' %
+                    ' '.join(['%smm' % str(trim[k]) for k in [
+                            'left',
+                            'bottom',
+                            'right',
+                            'top',
+                    ]])
+            )
+        # register image
+        uid = '%s_image' % context.UID()
+        view.addImage(uid=uid, image=graphic)
+        # generate latex
+        if command=='figure':
+            write(r'\begin{figure}[htbp]')
+        elif command=='wrapfigure':
+            write(r'\begin{wrapfigure}{hl}{%s}' % width)
+        write(r'\begin{center}')
+        write(r'\includegraphics[%s]{%s}' % (','.join(include_options), uid))
+        write(r'\end{center}')
+        write(r'\end{%s}' % command)
+        # register additional packages
+        view.conditionalRegisterPackage('graphicx')
+        view.conditionalRegisterPackage('wrapfig')
+        return '\n'.join(latex)
+
