e890f9e5e539be04a465369c5e00e2659581c7b4
Jonas Baumann <jone@jone.ch>
2011-08-09 19:47:19 +0200

    renamed i18n message factory to _

--- ftwbook/graphicblock/__init__.py
+++ ftwbook/graphicblock/__init__.py
@@ -7,12 +7,7 @@ from ftwbook.graphicblock import config
 from zope.i18nmessageid import MessageFactory
 
 
-# Define a message factory for when this product is internationalised.
-# This will be imported with the special name "_" in most modules. Strings
-# like _(u"message") will then be extracted by i18n tools for translation.
-
-
-graphicblockMessageFactory = MessageFactory('ftwbook.graphicblock')
+_ = MessageFactory('ftwbook.graphicblock')
 
 
 def initialize(context):
