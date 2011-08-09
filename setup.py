cb6eae6fc9d1b3b6c588d3f15641cbd03b801751
Jonas Baumann <jone@jone.ch>
2011-08-09 19:20:10 +0200

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,67 +1,60 @@
-# -*- coding: utf-8 -*-
-"""
-This module contains the tool of ftwbook.graphicblock
-"""
 from setuptools import setup, find_packages
+import os
 
-def read(*rnames):
-    return open('/'.join(rnames)).read()
 
 version = open('ftwbook/graphicblock/version.txt').read().strip()
 maintainer = 'Jonas Baumann'
 
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('docs', 'HISTORY.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('ftwbook', 'graphicblock', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
 
-tests_require=['zope.testing']
+tests_require = [
+    'zope.testing'
+    ]
+
 
 setup(name='ftwbook.graphicblock',
       version=version,
-      description="ftwbook News (Maintainer: %s)" % maintainer,
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='Addon for `ftw.book` providing an additional ' + \
+          'graphicblock for including PDF documents in the book. ' + \
+          '(Maintainer: %s)' % maintainer,
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       classifiers=[
+        'Environment :: Web Environment',
         'Framework :: Plone',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Zope2',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Printing',
         ],
-      keywords='',
+
+      keywords='ftw book graphicblock pdf',
       author='%s, 4teamwork GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/kunden/ftwbook/ftwbook.graphicblock/',
+      url='http://git.4teamwork.ch/',
+
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftwbook', ],
+
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        'plone.browserlayer',
-                        ],
+
+      install_requires=[
+        'setuptools',
+        'ftw.book',
+        'simplelayout.types.common',
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      test_suite = 'ftwbook.graphicblock.tests.test_docs.test_suite',
-      entry_points=""""""
 
+      test_suite = 'ftwbook.graphicblock.tests.test_docs.test_suite',
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
       )
