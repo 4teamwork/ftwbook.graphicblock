76f3cb2dfdc6d3cb67fea715836543905a33e74d
Jonas Baumann <jone@jone.ch>
2011-08-09 19:31:49 +0200

    removed viewlets

--- ftwbook/graphicblock/browser/viewlets.py
+++
@@ -1,7 +0,0 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from plone.app.layout.viewlets import ViewletBase
-
-class SimpleLayoutGraphicBlockViewlet(ViewletBase):
-    render = ViewPageTemplateFile('graphicblock_content.pt')
-
