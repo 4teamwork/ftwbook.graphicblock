d32359bd9367aeb2b74c67e8cd31f7a4a032abe7
Matthias Osswald <m@osswald.li>
2019-11-20 10:22:20 +0100

    Ignore modules when freezing time.
    ZODB will raise errors if it is not listed in the ignore_modules when using freeze.

--- ftwbook/graphicblock/testing.py
+++ ftwbook/graphicblock/testing.py
@@ -4,11 +4,12 @@ from ftw.builder import Builder
 from ftw.builder import session
 from ftw.builder import ticking_creator
 from ftw.builder.testing import functional_session_factory
+from ftw.testing import IS_PLONE_5
 from ftw.testing import freeze
 from ftw.testing.layer import COMPONENT_REGISTRY_ISOLATION
-from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
 from plone.testing import z2
 from zope.configuration import xmlconfig
 import ftw.book.tests.builders  # noqa
@@ -46,13 +47,13 @@ class GraphicblockLayer(PloneSandboxLayer):
             self.create_example_book().getPhysicalPath())
 
     def create_example_book(self):
-        with freeze(datetime(2016, 10, 31, 9, 52, 34)) as clock:
+        with freeze(datetime(2016, 10, 31, 9, 52, 34),
+                    ignore_modules=('ZODB.utils')) as clock:
             create = ticking_creator(clock, hours=1)
 
             book = create(
                 Builder('book')
                 .titled(u'The Example Book'))
-            # get_layout_behavior_registration(book)
 
             introduction = create(Builder('chapter').within(book)
                                   .titled(u'Introduction'))
